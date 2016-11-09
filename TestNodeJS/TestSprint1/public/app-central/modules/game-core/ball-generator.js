097c84b662bd1fae9c9329a2a1f1d5532ab0d93b
OnelieKata <oneliekata@gmail.com>
2016-11-09 16:30:46 +0100

    merge colliding

--- TestNodeJS/TestSprint1/public/app-central/modules/game-core/ball-generator.js
+++ TestNodeJS/TestSprint1/public/app-central/modules/game-core/ball-generator.js
@@ -7,10 +7,21 @@ ihmApp.factory('ballsGenerator', [ '$rootScope',
 				height : 600
 			};
 			var fps = 60;
-            //t: 1=rebondissante,2=cylindre,3=comette
-			list_balls["ball1"]={name:'ball1',x:300,y:300,vx:100,vy:0,r:50,t:3,c:'#FFFFFF',ct:'#000000'};
-            list_balls["ball2"]={name:'ball2',x:-26,y:-26,vx:100,vy:100,r:26,t:3,c:'#FF00FF',ct:'#000000'};
-            list_balls["ball3"]={name:'ball3',x:100,y:-26,vx:0,vy:200,r:26,t:3,c:'#FF00FF',ct:'#000000'};
+            list_balls["ball1"] = {
+				name : 'ball1',
+				x : 300,
+				y : 27,
+				vx : 300,
+				vy : 200,
+				r : 26,
+				t : 1, //1=rebondissante,2=cylindre,3=comette
+				bonus : 1, //0=ennemis,1= reverse,2=big padle 3 = bonus points
+				c : '#FFFFFF',//main color
+				ct : '#000000'//outside color 
+			};
+            
+            list_balls["ball2"]={name:'ball2',x:-26,y:-26,vx:100,vy:100,r:26,t:3,bonus:0,c:'#FF00FF',ct:'#000000'};
+            list_balls["ball3"]={name:'ball3',x:100,y:-26,vx:0,vy:200,r:26,t:3,bonus:0,c:'#FF00FF',ct:'#000000'};
 
  //           list_balls["ball2"]={name:'ball2',x:300,y:300,vx:0,vy:-50,r:26,t:1,c:'#000000',ct:'#FFFFFF'};
 //			list_balls["ball2"]={name:'ball2',x:500,y:27,vx:150,vy:-150,r:26,t:2,c:'#FF0000',ct:'#00FFFF'};
@@ -42,32 +53,39 @@ ihmApp.factory('ballsGenerator', [ '$rootScope',
                 var vyBall = Math.floor((Math.random() * (vyMax-vyMin)) + vyMin);
                 
                 
+                var bonusBall=0;
+                
                 var couleurMin=0;
                 var couleurMax=255;
                 
                 
-				list_balls["ball"+numeroBall]={name:"ball"+numeroBall,x:xBall,y:yBall,vx:vxBall,vy:vyBall,r:rBall,t:3,c:'#FF0000',ct:'#000000'};
+				list_balls["ball"+numeroBall]={name:"ball"+numeroBall,x:xBall,y:yBall,vx:vxBall,vy:vyBall,r:rBall,t:3,bonus:bonusBall,c:'#FF0000',ct:'#000000'};
                 numeroBall++;
 				
 			}
 			
 			var colliding = function(player) {
-				//todo
 				angular.forEach(list_balls, function(ball, key) {
+					if(pDistance(ball.x,ball.y,player.x,player.y,player.x+player.l,player.y) < ball.r || 
+					pDistance(ball.x,ball.y,player.x,player.y,player.x,player.y+10) < ball.r ||
+					pDistance(ball.x,ball.y,player.x,player.y+10,player.x+player.l,player.y+10) < ball.r ||
+					pDistance(ball.x,ball.y,player.x+player.l,player.y,player.x+player.l,player.y+10) < ball.r){
 					
-					//Ca touche mauvais
-					if (false) {
-						$rootScope.$broadcast('playerDamaged',player.name);
-					}
-					//Ca touche pouvoir 1 ou n
-					if (false) {
-						//Syntaxe donnation pouvoir
-						var powerAwarded = new Object();
-						powerAwarded['name']=player.name;
-						powerAwarded['type']=1;
-						var message = angular.toJson(powerAwarded);
-						$rootScope.$broadcast('givePowerToPlayer',message);
-					}
+						//Ca touche mauvais
+						if (ball.bonus==0) {
+							$rootScope.$broadcast('playerDamaged',player.name);
+						}
+						//Ca touche pouvoir 1 ou n
+						else {
+							//Syntaxe donnation pouvoir
+							var powerAwarded = new Object();
+							powerAwarded['name']=player.name;
+							powerAwarded['type']=ball.bonus;
+							var message = angular.toJson(powerAwarded);
+							$rootScope.$broadcast('givePowerToPlayer',message);
+						}
+                        delete list_balls[ball.name];
+					}	
 				});
 			}
 			
@@ -155,4 +173,37 @@ ihmApp.factory('ballsGenerator', [ '$rootScope',
 				moveBalls : moveBalls,
 				colliding : colliding
 			};
+            
+            //////////////////////////
+			
+			function pDistance(x, y, x1, y1, x2, y2) {
+			  var A = x - x1;
+			  var B = y - y1;
+			  var C = x2 - x1;
+			  var D = y2 - y1;
+			  var dot = A * C + B * D;
+			  var len_sq = C * C + D * D;
+			  var param = -1;
+			  if (len_sq != 0) //in case of 0 length line
+				  param = dot / len_sq;
+			  var xx, yy;
+			  if (param < 0) {
+				xx = x1;
+				yy = y1;
+			  }
+			  else if (param > 1) {
+				xx = x2;
+				yy = y2;
+			  }
+			  else {
+				xx = x1 + param * C;
+				yy = y1 + param * D;
+			  }
+			  var dx = x - xx;
+			  var dy = y - yy;
+			  return Math.sqrt(dx * dx + dy * dy);
+			}
+			
+			//////////////////////////
+            
 		} ]);
